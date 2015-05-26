ddcb2bdd40b008a0e777fc2ebbf45b47c0f64761
parth.mudgal <artpar@gmail.com>
2015-05-26 17:08:30 +0530

    all the changes until now

---
+++ 1.sql
@@ -0,0 +1,8 @@
+SELECT
+/* comment 1*/
+/* comment 2 */
+
+  (t.txn_id) txn_id,
+  max(t.amount), (t.stamp_created), 3+4, 6+5+3, concat(t.txn_id, t.txn_time) AS max_acount, t.amount + 100
+FROM data t
+GROUP BY stamp_created
